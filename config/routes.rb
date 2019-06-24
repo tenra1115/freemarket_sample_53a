24e8339288fa565d8376f86c251331828857dd2c
tenra1115 <katsuten1115@icloud.com>
2019-06-24 19:19:10 +0900

    make-routes.rb-and-some-controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+
+  root "products#index"
+  resources :users, only: [:index]
+  resources :products, only: [:index]
+  resources :comments, only: [:index]
+  resources :images, only: [:index]
+  resources :cards, only: [:index]
+  resources :addresses, only: [:index]
 end

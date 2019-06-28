a2288b3a400981252a5d5d639b4460d42bea4471
tenra1115 <katsuten1115@icloud.com>
2019-06-28 18:21:54 +0900

    remake-basic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,10 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth, if: :production?
+  before_action :basic_auth, if: :Rails.env.production?
   protect_from_forgery with: :exception
 
   private
-
-  def production?
-    Rails.env.production?
-  end
+    
+ 
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|

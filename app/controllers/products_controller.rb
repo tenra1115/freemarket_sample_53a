bc6c2b083d00e20ec453ae718322dfb775550aa7
nonodes <laopilove2@yahoo.co.jp>
2019-06-30 19:54:33 +0900

    create_sidebar_1

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,5 +2,15 @@ class ProductsController < ApplicationController
 
   def index
   end
+
+  def members
+  end
   
+  def members2
+  end
+
+  def members3
+  end
+  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
 
   root "products#index"
+  get 'products/members'  =>  'products#members'  
   resources :users, only: [:index]
   resources :products, only: [:index]
   resources :comments, only: [:index]
   resources :images, only: [:index]
   resources :cards, only: [:index]
   resources :addresses, only: [:index]
+  
 end
+

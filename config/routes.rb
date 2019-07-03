d888ffdbcf6a13e2b0b8812a7a31c5ae9be26dc7
tenra1115 <katsuten1115@icloud.com>
2019-07-03 19:28:04 +0900

    change-routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,4 @@ Rails.application.routes.draw do
   resources :images, only: [:index]
   resources :cards, only: [:index]
   resources :addresses, only: [:index]
-  get "users/new6" => "users#new6"
-  get "users/new5" => "users#new5"
-  get "users/new4" => "users#new4"
-  get "users/new3" => "users#new3"
-  get "users/new2" => "users#new2"
-  get "users/new1" => "users#new1"
-  get "users/new0" => "users#new0"
 end

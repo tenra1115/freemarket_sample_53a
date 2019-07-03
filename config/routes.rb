da7f705b1f2eac797d161ecaff191269001c09b3
sochisanchan <sochi19990403@gmail.com>
2019-07-03 19:28:18 +0900

    routes_change

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root "products#buy"
+  root "products#index"
   resources :users, only: [:index]
   resources :products, only: [:index]
   resources :comments, only: [:index]

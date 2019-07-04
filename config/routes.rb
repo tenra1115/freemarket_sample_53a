4946acad1926884dcc52192905a205cdccba876d
nonodes <laopilove2@yahoo.co.jp>
2019-07-04 16:25:19 +0900

    resolve-conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
 
-  root "products#index" 
-  resources :users, only: [:index]
+  root "products#index"
+  resources :users, only: [:index, :new]
   resources :products, only: [:index]
   resources :comments, only: [:index]
   resources :images, only: [:index]
   resources :cards, only: [:index]
   resources :addresses, only: [:index]
-  
+  get "users/base" => "users#base"
+  get "users/card" => "users#card"
+  get "users/privacy" => "users#privacy"
 end
 

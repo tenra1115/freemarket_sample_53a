7ebb0bff6bfb473c1d72d027e7e82bb001b05fa9
nonodes <laopilove2@yahoo.co.jp>
2019-07-11 12:06:24 +0900

    create_user.rb_validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,22 @@
 class User < ApplicationRecord
   devise :omniauthable, omniauth_providers: %i[google_oauth2 facebook]
+  
+  validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+
+  validates :encrypted_password, length: { minimum: 6 } 
+
+  validates :nickname, length: { minimum: 1, maximum: 10 }
+
+  validates :first_name, presence: true
+
+  validates :last_name, presence: true
+
+  validates :kana_first_name, presence: true
+
+  validates :kana_last_name, presence: true
+
+  validates :birth_date, presence: true
+
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
@@ -12,4 +29,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
 end

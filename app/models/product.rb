44569708961b2c3594c39216021f466f5912e09a
nonodes <laopilove2@yahoo.co.jp>
2019-07-11 20:56:04 +0900

    create_validates_error_message

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,8 @@
 class Product < ApplicationRecord
+
+  validates :name, length: { minimum: 1, maximum: 5}
+
+  validates :text, presence: true
+
+  validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 10000}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,19 @@
 class User < ApplicationRecord
   devise :omniauthable, omniauth_providers: %i[google_oauth2 facebook]
   
-  validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, on: :create }
 
   validates :encrypted_password, length: { minimum: 6 } 
 
-  validates :nickname, length: { minimum: 1, maximum: 10 }
+  validates :nickname, length: { minimum: 1, maximum: 5 }
 
-  validates :first_name, presence: true
+  validates :first_name, presence: true, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/}
 
-  validates :last_name, presence: true
+  validates :last_name, presence: true, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/}
 
-  validates :kana_first_name, presence: true
+  validates :kana_first_name, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
 
-  validates :kana_last_name, presence: true
+  validates :kana_last_name, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
 
   validates :birth_date, presence: true
 

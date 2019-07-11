8e2d004afe4a583a0fe4a3f8d8e8afb15b9297dd
tenra1115 <katsuten1115@icloud.com>
2019-07-11 19:49:19 +0900

    make-sns-login

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -11,6 +11,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   # common callback method
   def callback_for(provider)
+    # ユーザーの作成
     @user = User.from_omniauth(request.env["omniauth.auth"])
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated

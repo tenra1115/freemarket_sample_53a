4e583ad3cf99a91ab0206f280e5b0cb17497dcdd
sochisanchan <sochi19990403@gmail.com>
2019-07-12 14:11:29 +0900

    display-toppage,routes-change

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :authenticate_user! 
+  # before_action :authenticate_user! 
   protect_from_forgery with: :exception
   
 

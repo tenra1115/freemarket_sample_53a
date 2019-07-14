78c869a1e067c174014e04d9ff7718303c2f9c8c
tenra1115 <katsuten1115@icloud.com>
2019-07-14 16:05:03 +0900

    delete-code

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,14 +9,6 @@ class User < ApplicationRecord
 
   validates :nickname, length: { minimum: 1, maximum: 5 }
 
-  # validates :first_name, presence: true, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/}
-
-  # validates :last_name, presence: true, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/}
-
-  # validates :kana_first_name, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
-
-  # validates :kana_last_name, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
-
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email

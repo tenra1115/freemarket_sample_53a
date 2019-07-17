4b57bc3a20f6ef4421b7bd21bb138d9a4d315da0
nonodes <laopilove2@yahoo.co.jp>
2019-07-17 10:41:20 +0900

    Fix_pankuduroot_detail_link

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -29,7 +29,7 @@ class ProductsController < ApplicationController
 
   def detail
     @user = User.find(current_user.id)
-    @product = Product.find(params[:id])
+    @product = Product.find_by(params[current_user.id])
   end
 
   def edit

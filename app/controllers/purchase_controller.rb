e8afbba92c93ed64e946eb04088bf33ef527868d
tenra1115 <katsuten1115@icloud.com>
2019-07-18 15:41:49 +0900

    remake-payjp

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -13,7 +13,7 @@ class PurchaseController < ApplicationController
       #登録された情報がない場合にカード登録画面に移動
       redirect_to controller: "card", action: "new"
     else
-      Payjp.api_key = "sk_test_10114eafe2605308cd83bd02"
+      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
       #保管した顧客IDでpayjpから情報取得
       customer = Payjp::Customer.retrieve(card.customer_id)
       #保管したカードIDでpayjpから情報取得、カード情報表示のためインスタンス変数に代入
@@ -29,7 +29,7 @@ class PurchaseController < ApplicationController
 
     
     card = Card.where(user_id: current_user.id).first
-    Payjp.api_key = "sk_test_10114eafe2605308cd83bd02"
+    Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
     charge = Payjp::Charge.create(
       :amount => product.price,
       :customer => card.customer_id,

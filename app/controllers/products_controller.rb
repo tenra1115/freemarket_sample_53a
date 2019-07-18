a7d58929bfcdb93e98a96488490d46dffc74ec15
tenra1115 <katsuten1115@icloud.com>
2019-07-18 15:23:09 +0900

    remake

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
 
-  before_action :authenticate_user!, only: [:show]
+  before_action :authenticate_user!, except: [:index, :show]
   
   def index
     @products = Product.limit(4)
